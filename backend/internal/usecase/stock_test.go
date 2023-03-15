7e72edba77b4434189de21158838da3c0811b59f
Allan Cordeiro <allan.cordeiro.santos@gmail.com>
2023-03-15 17:00:00 -0300

    feat: add stock usecase unit tests

--- backend/internal/usecase/stock_test.go
+++ backend/internal/usecase/stock_test.go
@@ -1,6 +1,8 @@
 package usecase
 
 import (
+	"errors"
+	"fmt"
 	"github.com/AllanCordeiro/impacta-alpha-despensa/internal/domain/entity"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
@@ -57,21 +59,76 @@ func TestCreateProductUseCase_Execute(t *testing.T) {
 			expectedErrNumber: 1,
 			expectedErr:       entity.ErrInvalidQuantity,
 		},
+		{
+			name:              "given quantity greater than 32600 when execute CreateProductUseCase should return an error",
+			product:           "product 1",
+			creationDate:      time.Now().Format("2006-01-02"),
+			quantity:          35000,
+			expirationDate:    time.Now().Add(time.Hour * 24 * 5).Format("2006-01-02"),
+			isShouldBeOk:      true,
+			expectedErrNumber: 1,
+			expectedErr:       entity.ErrInvalidQuantity,
+		},
+		{
+			name:              "given incorrect creation date when execute CreateProductUseCase should return an error",
+			product:           "product 1",
+			creationDate:      "20/07/2012",
+			quantity:          -15,
+			expirationDate:    time.Now().Add(time.Hour * 24 * 5).Format("2006-01-02"),
+			isShouldBeOk:      true,
+			expectedErrNumber: 1,
+			expectedErr:       entity.ErrInvalidCreationDate,
+		},
+		{
+			name:              "given quantity greater than 32600 when execute CreateProductUseCase should return an error",
+			product:           "product 1",
+			creationDate:      time.Now().Format("2006-01-02"),
+			quantity:          35000,
+			expirationDate:    "20/07/2077",
+			isShouldBeOk:      true,
+			expectedErrNumber: 1,
+			expectedErr:       entity.ErrInvalidExpirationDate,
+		},
 	}
 
 	for _, tt := range brScenarios {
-		m.On("Save", mock.Anything).Return(nil)
+		t.Run(tt.name, func(t *testing.T) {
+			m.On("Save", mock.Anything).Return(nil)
+			uc := NewCreateProductUseCase(m)
+			input := CreateProductInput{
+				Name:           tt.product,
+				CreationDate:   tt.creationDate,
+				Quantity:       tt.quantity,
+				ExpirationDate: tt.expirationDate,
+			}
+			output := uc.Execute(input)
+			assert.Equal(t, tt.expectedErrNumber, len(output.Msgs))
+			for _, errMsg := range output.Msgs {
+				assert.Equal(t, tt.expectedErr, errMsg.Err)
+			}
+			m.AssertExpectations(t)
+			m.AssertNumberOfCalls(t, "Save", 1)
+		})
+	}
+
+	t.Run("given a valid data when execute CreateProductUseCase but an gateway error comes in should return an internal error", func(t *testing.T) {
+		m := &StockGatewayMock{}
+		m.On("Save", mock.Anything).Return(errors.New("sql: database is closed"))
 		uc := NewCreateProductUseCase(m)
 		input := CreateProductInput{
-			Name:           tt.product,
-			CreationDate:   tt.creationDate,
-			Quantity:       tt.quantity,
-			ExpirationDate: tt.expirationDate,
+			Name:           "product 1",
+			CreationDate:   time.Now().Format("2006-01-02"),
+			Quantity:       2,
+			ExpirationDate: time.Now().Add(time.Hour * 24 * 5).Format("2006-01-02"),
 		}
 		output := uc.Execute(input)
-		assert.Equal(t, tt.expectedErrNumber, len(output.Msgs))
+		fmt.Println(output)
+		assert.NotNil(t, output.Msgs)
 		for _, errMsg := range output.Msgs {
-			assert.Equal(t, tt.expectedErr, errMsg.Err)
+			assert.Equal(t, "internal", errMsg.Entity)
+			assert.Equal(t, "sql: database is closed", errMsg.Err.Error())
 		}
-	}
+		m.AssertExpectations(t)
+		m.AssertNumberOfCalls(t, "Save", 1)
+	})
 }
