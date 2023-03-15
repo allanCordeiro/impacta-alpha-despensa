a1e016705159f169eb7a02069022baf2f1471dea
Allan Cordeiro <allan.cordeiro.santos@gmail.com>
2023-03-15 00:49:04 -0300

    feat: add stock gateway

---
+++ backend/internal/domain/gateway/stock.go
@@ -0,0 +1,8 @@
+package gateway
+
+import "github.com/AllanCordeiro/impacta-alpha-despensa/internal/domain/entity"
+
+type StockGateway interface {
+	Save(stock entity.Product) error
+	GetProducts() []entity.Product
+}
