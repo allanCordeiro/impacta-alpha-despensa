63ef10ac2c4a6d4a3c11bbae5ba62ce0865a42c1
Allan Cordeiro <allan.cordeiro.santos@gmail.com>
2023-03-19 16:48:17 -0300

    feat: add GET endpoint to see all products stored in db

--- backend/internal/domain/gateway/stock.go
+++ backend/internal/domain/gateway/stock.go
@@ -5,4 +5,5 @@ import "github.com/AllanCordeiro/impacta-alpha-despensa/internal/domain/entity"
 type StockGateway interface {
 	Save(stock *entity.Product) error
 	GetByID(id string) (*entity.Product, error)
+	GetAllProducts() ([]entity.Product, error)
 }
---
+++ backend/internal/usecase/stock_get.go
@@ -0,0 +1,48 @@
+package usecase
+
+import (
+	"github.com/AllanCordeiro/impacta-alpha-despensa/internal/domain/gateway"
+	"log"
+)
+
+type GetProductInput struct {
+}
+
+type GetProductOutput struct {
+	ID             string `json:"id"`
+	Name           string `json:"name"`
+	CreationDate   string `json:"creation_date"`
+	Quantity       int    `json:"quantity"`
+	ExpirationDate string `json:"expiration_date"`
+}
+
+type GetProductUseCase struct {
+	StockGateway gateway.StockGateway
+}
+
+func NewGetProductUseCase(stockGateway gateway.StockGateway) *GetProductUseCase {
+	return &GetProductUseCase{
+		StockGateway: stockGateway,
+	}
+}
+
+func (p *GetProductUseCase) Execute() []GetProductOutput {
+	entities, err := p.StockGateway.GetAllProducts()
+	if err != nil {
+		log.Println(err)
+		//todo better way to send an error msg to front
+	}
+
+	var products []GetProductOutput
+	for _, prd := range entities {
+		product := &GetProductOutput{
+			ID:             prd.ID,
+			Name:           prd.Name,
+			CreationDate:   prd.CreationDate.String(),
+			Quantity:       prd.Quantity,
+			ExpirationDate: prd.ExpirationDate.String(),
+		}
+		products = append(products, *product)
+	}
+	return products
+}
